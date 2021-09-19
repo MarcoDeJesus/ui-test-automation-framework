74464fa2ec39926c5202df6085b7f6f430beece2
Mardo De Jesus <marcodejesus2010@gmail.com>
2021-09-19 08:58:10 -0500

    Settig up Selenium WebDriver to run with FirefoxDriver (so called - GeckoDriver)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,23 @@
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
 
-        WebDriver driver = new ChromeDriver();
+/*         Below code could be summarized as:
+        new FirefoxOptions().setBinary(new FirefoxBinary(new File("src/main/resources/geckodriver")));*/
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        File geckoDriverFile = new File("src/main/resources/geckodriver");
+        FirefoxBinary firefoxBinary = new FirefoxBinary(geckoDriverFile);
+        firefoxOptions.setBinary(firefoxBinary);
+
+        WebDriver driver = new FirefoxDriver();
+
         driver.get("http://automationpractice.com/index.php");
     }
 }
