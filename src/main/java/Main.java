d5eb0c862265f236bab33eca163170d634239d59
Mardo De Jesus <marcodejesus2010@gmail.com>
2021-09-19 09:17:29 -0500

    Utilizing the selenium methods to get page title, current url and page source code.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,8 +12,17 @@ public class Main {
         // Open the Website to interact with.
         driver.get("http://automationpractice.com/index.php");
 
-        // Wait
-        Thread.sleep(1000);
+        // Printing page title
+        String pageTitle = driver.getTitle();
+        System.out.println("PAGE TITLE: " + pageTitle);
+
+        // Printing current url
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println("CURRENT URL: " + currentUrl);
+
+        // Printing source code
+        String pageSource = driver.getPageSource();
+        System.out.println("PAGE SOURCE: " + pageSource);
 
         // Close current window.
         driver.close();
