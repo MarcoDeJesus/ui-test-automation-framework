4fe4bf54d895f5fe301d31eac95df938bdeb6eb5
Mardo De Jesus <marcodejesus2010@gmail.com>
2021-09-19 10:18:31 -0500

    Identifying Website WebElements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
@@ -10,46 +12,25 @@ public class Main {
         WebDriver driver = new ChromeDriver();
 
         // Open the Website to interact with.
-        driver.get("https://www.google.com/");
+        driver.get("https://www.youtube.com/");
 
-        // Printing initial site url.
-        String currentUrl = driver.getCurrentUrl();
-        System.out.println("CURRENT URL: " + currentUrl);
+        // Find search bar element through XPATH= //input[@id='search']
+        WebElement searchBar = driver.findElement(By.xpath("//input[@id='search']"));
 
-        // Navigating to a different site and printing url.
-        driver.navigate().to("https://www.facebook.com/");
-        currentUrl = driver.getCurrentUrl();
-        System.out.println("CURRENT URL: " + currentUrl);
+        // Check if element is displayed and enabled
+        if(searchBar.isDisplayed() && searchBar.isEnabled()){
 
-        // Navigating to a different site and printing url.
-        driver.navigate().to("https://www.espn.com/");
-        currentUrl = driver.getCurrentUrl();
-        System.out.println("CURRENT URL: " + currentUrl);
+            // Type Selenium
+            searchBar.sendKeys("Selenium");
 
-        // Moving backward twice and print current url.
-        driver.navigate().back();
-        driver.navigate().back();
-        currentUrl = driver.getCurrentUrl();
-        System.out.println("CURRENT URL: " + currentUrl);
+            // Find search button element: ID = search-icon-legacy
+            WebElement searchButton = driver.findElement(By.id("search-icon-legacy"));
+            // Click button
+            searchButton.click();
+        }
 
-        // Moving forward once and print current url.
-        driver.navigate().forward();
-        currentUrl = driver.getCurrentUrl();
-        System.out.println("CURRENT URL: " + currentUrl);
-
-        // Asserting that current url is Facebook - DO NOT FORGET TO ENABLE ASSERTIONS FROM YOUR IDEA CONFIGURATION
-        // VM options =  -ea
-        // VM options =  -enableassertions
-        assert currentUrl.equals("https://www.facebook.com/") : "Page is incorrect.";
-        /*
-        assert line could be replaced with a if statement.
-         if (!currentUrl.equals("https://www.facebook.com/")) throw new AssertionError("Page is incorrect.");
-        * */
-
-        // Refreshing the url
-        driver.navigate().refresh();
-        currentUrl = driver.getCurrentUrl();
-        System.out.println("CURRENT URL: " + currentUrl);
+        // Wait 5 seconds
+        Thread.sleep(5000);
 
         // Close current window.
         driver.close();
