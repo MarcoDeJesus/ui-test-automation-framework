d8a1bafd3d48464fe48093e3e4023d123d46e94c
Mardo De Jesus <marcodejesus2010@gmail.com>
2021-09-19 10:57:21 -0500

    Creating a custom exception to be thrown when no webdriver has been implemented.

---
+++ src/main/java/org/mdejesus/spectral/exceptions/NotWebDriverImplementedException.java
@@ -0,0 +1,7 @@
+package org.mdejesus.spectral.exceptions;
+
+public class NotWebDriverImplementedException extends Exception {
+    public NotWebDriverImplementedException(String message){
+        super(message);
+    }
+}
