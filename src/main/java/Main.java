82be4dcaaae69411f08e197e1f90e13d2a6b95fe
Mardo De Jesus <marcodejesus2010@gmail.com>
2021-09-19 09:09:37 -0500

    Adding the Close functionality to close the Browser Window.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        // Setup the System properties with ChromeDriver information.
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 
+        // Create an instance of ChromeDriver.
         WebDriver driver = new ChromeDriver();
+
+        // Open the Website to interact with.
         driver.get("http://automationpractice.com/index.php");
+
+        // Wait
+        Thread.sleep(1000);
+
+        // Close current window.
+        driver.close();
     }
 }
