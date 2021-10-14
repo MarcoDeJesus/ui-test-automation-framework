2281f2255d6cb272de4180f6b234e42daef30add
Marco De Jesus <marcodejesus2010@gmail.com>
2021-10-13 20:09:05 -0500

    Implementing TestNG - Exercise 1

---
+++ src/test/java/com/qalabs/pageobjects/TestNGExercise1.java
@@ -0,0 +1,27 @@
+package com.qalabs.pageobjects;
+
+import org.mdejesus.spectral.WebDriverManager;
+import org.mdejesus.spectral.exceptions.NotWebDriverImplementedException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestNGExercise1 {
+    @Test
+    public void validateGoogleUrl() throws NotWebDriverImplementedException {
+        WebDriver driver = WebDriverManager.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://www.google.com.mx/");
+
+        String expected = "https://www.google.com.mx/?gws_rd=ssl";
+
+        String actual = driver.getCurrentUrl();
+
+        assertEquals(expected, actual, "Url is incorrect.");
+    }
+}
