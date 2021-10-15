fce1c7ad6a7d0887a4f09b28af8097c2590dad7c
Marco De Jesus <marcodejesus2010@gmail.com>
2021-10-15 01:18:41 -0500

    Example GoolgeSignIn Page Object - slide 21.

---
+++ src/test/java/com/qalabs/pageobjects/GoogleSignIn.java
@@ -0,0 +1,16 @@
+package com.qalabs.pageobjects;
+
+import org.openqa.selenium.WebElement;
+
+public class GoogleSignIn {
+    private WebElement emailTextBox;
+    private WebElement forgetLink;
+    private WebElement moreOptions;
+    private WebElement nextButton;
+
+    public void clickNext(){}
+    public void setEMail(String in){}
+    public void clickForgotEmail(){}
+    public void clickMoreOptions(){}
+    public Boolean isLoaded(){return false;}
+}
